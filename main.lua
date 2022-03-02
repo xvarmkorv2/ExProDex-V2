252e25e3240272c20a3575e4cd2ee6b34ccdd91b
Madi <49875377+L8X@users.noreply.github.com>
2022-03-02 12:38:03 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/L8X/ExProDex-V2/main/src.lua", false))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/L8X/ExProDex-V2/main/src.lua", false))()
