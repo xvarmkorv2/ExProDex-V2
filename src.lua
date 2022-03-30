b422939ac4539264c51febd92828521be049c717
Madi <49875377+L8X@users.noreply.github.com>
2022-03-30 14:08:56 +0100

    Update src.lua

--- src.lua
+++ src.lua
@@ -13,32 +13,6 @@ getgenv().EXPRODEX_LOADED = true
 
 
 pcall(function()
-
-local OldIndex
-OldIndex = hookmetamethod(game, "__index", function(Self, Index)
-    return OldIndex(Self, Index)
-end)
-
-local OldNewIndex
-OldNewIndex = hookmetamethod(game, "__newindex", function(Self, Index, Value)
-    return OldNewIndex(Self, Index, Value)
-end)
-
-local OldNamecall
-OldNamecall = hookmetamethod(game, "__namecall", function(Self, ...)
-    return OldNamecall(Self, ...)
-end)
-
-local mt = getrawmetatable(game)
-
-local old
-old = hookfunction(mt.__namecall, function(...)
-   return old(...)
-end)
-
-end)
-
-pcall(function()
 if identifyexecutor() == "ScriptWare" then
 local function decomp(a)
     return tostring(disassemble(getscriptbytecode(a)))
